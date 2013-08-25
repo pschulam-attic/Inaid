60144bb017088c06bf6191b1c5bbb30405a79e18
Peter Schulam <pschulam@gmail.com>
2013-08-25 17:13:33 -0400

    Add plots for exploratory analysis.

--- Work/2013_08/experiment.R
+++ Work/2013_08/experiment.R
@@ -1,13 +1,11 @@
 library(plyr)
 
-source("R/preprocess.R")
-
 predict.prognosis <- function(df) {
     fit <- lm(fvc ~ year, data=df)
     coef(fit)[2] < 0
 }
 
-data <- read.orig()
+data <- arrange(read.csv("../../Data/fvc.5.visits.csv"), PtID, visit)
 
 results <- ddply(data, .(PtID), summarize, prognosis=fvc[1] > fvc[5])
 predictions <- by(data, factor(data$PtID), predict.prognosis)
